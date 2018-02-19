0387f29b445785e33abbf9151be98baa0f37c10d
Antonio Navajas Ojeda <ajnavajas84@gmail.com>
2018-02-19 15:09:52 +0100

    Revert "Merge branch 'master' of https://github.com/antonionavajas/Coach-App"
    This reverts commit 0d1af3fb520b883ef12f2d5fabfa7bcebe75f9f9, reversing
    changes made to 74acd9ed16ed77cbfbd924ecb7b68ea0a08e7e20.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,10 @@
 import { Component } from '@angular/core';
-import { CaNavComponent } from './shared/ca-nav/ca-nav.component';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css'],
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-
+  title = 'app';
 }
